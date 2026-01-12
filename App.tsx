4376a81d0ba415f4fe9be2444ab012818381ecab
zyanmaahin133 <zyanmaahin133@gmail.com>
2026-01-12 18:56:34 +0530

    add this project

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AuthNavigator from './src/navigation/AuthNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Auth" component={AuthNavigator} />
+        <Stack.Screen name="App" component={AppNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
